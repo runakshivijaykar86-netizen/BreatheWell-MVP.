7124d5dde3b5ae26a0b38dc1686c5993954377e7
runakshivijaykar86-netizen <runakshivijaykar86@gmail.com>
2025-12-28 16:07:30 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,22 @@
-// sw.js - The Service Worker
+// sw.js - Service Worker for BreatheWell
 const CACHE_NAME = 'breathewell-v1';
 const ASSETS = [
   './',
   './index.html',
   './app.js',
-  'https://cdn.jsdelivr.net/npm/chart.js'
+  './manifest.json'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
